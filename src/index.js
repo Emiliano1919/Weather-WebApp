7c29bd68f417d3231d09d9c5533de6ad0ee7ba5f
Emiliano1919 <emilianojg38@gmail.com>
2023-11-11 21:26:05 -0500

    Add API functionality

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// URL (required), options (optional)
+fetch(
+  "https://api.weatherapi.com/v1/current.json?key=eadbfdb38fa148678c615752231211&q=Ottawa&aqi=no",
+  { mode: "cors" },
+)
+  .then((response) => {
+    console.log(response.json());
+  })
+  .catch((err) => {
+    console.log(err);
+  });
